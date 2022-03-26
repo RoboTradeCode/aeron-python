ae91b57ce2daaf3c5bb1a3731f545b58d81d2e71
nomnoms12 <alexander.ign0918@gmail.com>
2022-03-27 01:59:57 +0300

    Use setuptools instead distutils

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
-from distutils.core import setup, Extension
+import setuptools
 
-aeron = Extension('aeron',
-                  include_dirs=['libs/aeron/aeron-client/src/main/c'],
-                  libraries=['aeron'],
-                  library_dirs=['/usr/local/lib'],
-                  sources=['aeronmodule.c'])
+aeron = setuptools.Extension(
+    name='aeron',
+    include_dirs=['libs/aeron/aeron-client/src/main/c'],
+    libraries=['aeron'],
+    library_dirs=['/usr/local/lib'],
+    sources=['aeronmodule.c'],
+)
 
-setup(name='aeron',
-      version='0.1',
-      description='Efficient reliable UDP unicast, UDP multicast, and IPC message transport',
-      author='nomnoms12',
-      author_email='alexander.ign0918@gmail.com',
-      url='https://github.com/real-logic/aeron',
-      ext_modules=[aeron])
+with open('README.md', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='aeron',
+    version='0.0.1',
+    author='nomnoms12',
+    author_email='alexander.ign0918@gmail.com',
+    description='Efficient reliable UDP unicast, UDP multicast, and IPC message transport',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RoboTradeCode/aeron-python',
+    project_urls={
+        'Bug Tracker': 'https://github.com/RoboTradeCode/aeron-python/issues',
+    },
+    classifiers=[
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: C',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    ext_modules=[aeron],
+)
